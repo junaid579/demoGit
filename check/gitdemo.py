5d3a5ed176032ca19f19cbd50b1740e4940498fe
Khan <mkhan@digitallynctech.om>
2018-07-17 11:06:29 +0530

    third change

---
+++ check/gitdemo.py
@@ -0,0 +1,84 @@
+Git -- version control 
+--> repository management system 
+--> drive for code 
+--> devops 
+--------------------
+git ---> installed 
+google -- git --> download and run 
+git --> local machine 
+c/:>git 
+---
+---cmds
+---
+windows --> git + git GUI + git bash
+
+git --> technology 
+git GUI --> usage of git without commands 
+git bash --> shell / cmd for git 
+
+------------------------------
+git works --> files , folders(directories)
+
+-> pdf , code , image .....
+
+
+--> install
+--> initialise the repository/folder (local)
+	git init ( at folder location)
+	Initialized empty Git repository in /Users/junaid/Desktop/gitdemoKP/.git/
+
+3 divisions
+-----------
+working directory --> files/folder in local 
+staging area --> files go from working directory
+local repository --> .git
+
+100 systems --> server --> fb.com
+
+100 systems --> 100 local repository
+server --> global repository 
+	--> remote repository
+		2 types 
+			--> git server
+			--> third party services 
+				--> public remote repository (all can see)
+				--> github , bitbucket , gitlab
+
+public repository --> free
+private repository --> paid 
+
+github remote repository
+--> email , username , password 
+github.com/username --> remote repositories
+
+-------------
+git commands
+------------
+git init --> initialise the folder and creates 3 areas
+git add <filename> --> transfer the <filename> from working 						directory to staging area 
+git add . --> all files from working directory to staging area 
+git commit -m "<type any message>" --> send staging areas files to local repository
+git status --> status of initialised repository/folder
+
+On branch master
+nothing to commit, working tree clean
+working directory = local repository
+
+local repository ---> remote repository (github)
+connection. --> command will be given in github
+	git remote add origin <link to remote repository>
+	git remote add origin https://github.com/junaid579/demoGit.git
+git push origin master
+
+master -> local machine
+origin -> remote repository
+
+master -> master --> code got pushed to remote repository
+
+
+
+
+
+
+
+
